807788c29cfb036aea174d20d84027f516913548
ValerieLeonidova <47154479+ValerieLeonidova@users.noreply.github.com>
2022-02-04 11:37:11 +0200

    page add

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp')
+
+const plumber = require('gulp-plumber')
+const sass = require('gulp-sass')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
+const shorthand = require('gulp-shorthand')
+const autoprefixer = require('gulp-autoprefixer')
+
+
+gulp.task('styles', function (){
+    return gulp.src('./scss/**/style.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(shorthand())
+        .pipe(cleanCSS({
+            debug: true,
+            compatibility: '*'
+        }, details => {
+            console.log(`${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`)
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'))
+});
+
+gulp.task('watch', function (){
+    gulp.watch('./scss/**/*.scss', gulp.series('styles'));
+});
